d3d8e368e290098bebaa90cc898a252a47aad87f
IchBinBekifft <73794066+Clegames@users.noreply.github.com>
2022-07-03 12:01:56 +0200

    Added a new page

--- src/main/java/de/clemens/controller/ReadingSampleController.java
+++ src/main/java/de/clemens/controller/ReadingSampleController.java
@@ -4,10 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class ImprintController {
+public class ReadingSampleController {
 
-    @GetMapping("imprint")
-    public String get() {
-        return "imprint/imprint";
+    @GetMapping("/reading-sample")
+    public String getter() {
+        return "sample";
     }
 }
